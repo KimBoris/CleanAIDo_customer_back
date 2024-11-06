c4b3c551a6b897919148b7f9cfc4a445260e346d
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-11-06 09:11:29 +0900

    Initial commit

---
+++ src/main/java/org/zerock/cleanaido_customer_back/CleanAiDoCustomerBackApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.cleanaido_customer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanAiDoCustomerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanAiDoCustomerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/cleanaido_customer_back/CleanAiDoCustomerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.cleanaido_customer_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanAiDoCustomerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
