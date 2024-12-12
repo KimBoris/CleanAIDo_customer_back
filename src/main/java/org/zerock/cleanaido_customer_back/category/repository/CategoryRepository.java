c190a3c1d3fb38075f999d2ae125cbbac5880ab7
KimBoris <zlas2345@gmail.com>
2024-12-12 10:41:06 +0900

    KBK_Category category list 완료

--- src/main/java/org/zerock/cleanaido_customer_back/category/repository/CategoryRepository.java
+++ src/main/java/org/zerock/cleanaido_customer_back/category/repository/CategoryRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.zerock.cleanaido_customer_back.category.entity.Category;
+import org.zerock.cleanaido_customer_back.category.repository.search.CategorySearch;
 
 import java.util.List;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long>, CategorySearch {
 
     @Query("SELECT c FROM Category c WHERE c.cname = :name")
     List<Category> findByName(@Param("name") String name);
